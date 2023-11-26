d8ed307bfdd246381ecc274c15764fc11be7c041
Nate-is-Great <114320605+Nate-is-Great@users.noreply.github.com>
2023-11-26 06:59:31 -0800

    Update test_radio_button_steps_students.py
    Provided the starter code for students.

---
+++ For_Students/test_radio_button_steps_students.py
@@ -0,0 +1,43 @@
+from behave import *
+from selenium.webdriver.common.by import By
+
+
+@given(u'')
+def radio_button_4(context):
+    # assert the radio button is not selected by default
+    pass
+
+@when(u'')
+def select_radio_button_4(context):
+    #find element of website using a driver 
+    #click variable
+    pass
+
+   
+
+
+@then(u'')
+def display_selection(context):
+    # assert that the user selected Favorite Language: Python
+    pass
+
+@given(u'')
+def radio_button_1(context):
+    # assert radio button four is selected/clicked
+    # assert that "Favorite Language python" is displayed
+    pass
+
+
+
+@when(u'')
+def select_radio_button_1(context):
+    # User selects Radio Button 1 and clicks submit
+    pass
+    
+
+
+@then(u'')
+def display_selection(context):
+    # assert "Favorite Language: JavaScript" is displayed
+    pass
+    
